eeac6ed9b737b1391c0325a20e7fdeeb0fd6055e
gebaki <yamamoto.takaki.4985@members.co.jp>
2024-11-05 16:19:05 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.functions import col
+from snowflake.snowpark.functions import col, requests
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
@@ -28,6 +28,8 @@ if ingredients_list:
     #FORブロックを追加する
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen +' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
         
     #st.write(ingredients_string)
     
@@ -43,8 +45,5 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
-# New section to display fruityvice nutrition information
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+
 
